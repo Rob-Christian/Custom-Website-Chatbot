64a861003e87b06758e89c0946fdd4abb94df79c
Rob Christian Caduyac <59250293+Rob-Christian@users.noreply.github.com>
2024-12-29 00:09:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# Install necessary packages
+# Import necessary packages
 import os
 import streamlit as st
 from langchain.document_loaders import UnstructuredURLLoader
@@ -9,3 +9,19 @@ from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
 from langchain.chains import ConversationalRetrievalChain
 import validators
+
+# Load OpenAI API key
+os.environ["OPENAI_API_KEY"] = st.secrets("key")
+
+# Streamlit user interface
+st.title("Conversational Website Chatbot")
+st.write("Enter a valid website to start processing its contents")
+
+# Get URL
+url = st.text_input("Enter a website URL: ")
+
+if url:
+  if validators.url(url):
+    st.success("Valid URL. Processing the content from the website...")
+  else:
+    st.error("Invalid URL. Please enter a valid website")
