f401cd8f064af12eb2a11d47312ee25eb3aadc41
Rob Christian Caduyac <59250293+Rob-Christian@users.noreply.github.com>
2024-12-29 09:35:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,6 +37,25 @@ if url:
       embeddings = OpenAIEmbeddings()
       vectordb = FAISS.from_documents(documents = chunks, embedding = embeddings)
 
+      # Setup Prompt
+      template = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+
+      Chat History:
+      {chat_history}
+      Follow Up Input: {question}
+      Standalone question:"""
+
+      CONDENSE_QUESTION_PROMPT = PromptTemplate.from_template(template)
+
+      prompt_template = """Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+
+      {context}
+      
+      Question: {question}
+      Helpful Answer:"""
+
+      QA_PROMPT = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+
       # Setup LLM
       llm = ChatOpenAI(temperature = 0.2)
       memory = ConversationSummaryBufferMemory(
